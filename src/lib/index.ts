3a00426941add5c8506694d665f61a376c839654
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-07-13 19:52:10 +0200

    more exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,3 +8,16 @@ export {
 } from './server/handlers/verification-page-handler.js';
 export { googleLoginCallbackHandler } from './server/handlers/google-login-callback-handler.js';
 export { googleLoginHandler } from './server/handlers/google-login-handler.js';
+export { layoutLoadHandler } from './server/handlers/layout-load-handler.js';
+export { signupLoadHandler, signupActions } from './server/handlers/signup-handler.js';
+export { signoutHandler } from './server/handlers/signout-handler.js';
+export {
+	resetPasswordLoadHandler,
+	resetPasswordActions
+} from './server/handlers/reset-password-handler.js';
+export {
+	resetPasswordVerificationLoadHandler,
+	resetPasswordVerificationActions
+} from './server/handlers/reset-password-verification-handler.js';
+export { loginLoadHandler, loginActions } from './server/handlers/login-handler.js';
+export { forgotPasswordActions } from './server/handlers/forgot-password-handler.js';
