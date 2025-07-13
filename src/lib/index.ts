51fbe610af09c326713b2e6efe6ac71b660bfd30
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-07-13 19:03:49 +0200

    more exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,10 @@
 export { google } from './server/oauth.js';
 export { db } from './server/db/index.js';
+
+export { authHandler } from './server/handlers/auth-handler.js';
+export {
+	verificationLoadHandler,
+	verificationActions
+} from './server/handlers/verification-page-handler.js';
+export { googleLoginCallbackHandler } from './server/handlers/google-login-callback-handler.js';
+export { googleLoginHandler } from './server/handlers/google-login-handler.js';
